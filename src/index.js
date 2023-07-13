1846e9cd9fbfeaeff2b881b43f186e7983e5fbfa
Vanhawk587 <chandelgaurav587@gmail.com>
2023-07-14 00:30:30 +0530

    final - commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {ChakraProvider} from "@chakra-ui/react";
+import {BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { createRoot } from 'react-dom/client';
+import ChatProvider from './Context/ChatProvider';
+
+const container = document.getElementById('root');
+const root =createRoot(container);
+
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+  <ChatProvider>   
+    <ChakraProvider>
+        <App />
+    </ChakraProvider>
+    
+    
+   
+  </ChatProvider>
+  </BrowserRouter>
+ 
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+
+
